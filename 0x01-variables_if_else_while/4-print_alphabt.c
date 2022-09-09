6c3e6a956b1ef282deb0a2cbfd449e675f9bf7ff
Lukman Buhari <lukmanbuhari07@gmail.com>
2022-09-09 10:56:22 -0700

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *  main - prints the alphabets without q and e
+ *
+ *  Return: Always 0 (Successful)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && 1 != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n')
+		return (0);
+}
+
+
