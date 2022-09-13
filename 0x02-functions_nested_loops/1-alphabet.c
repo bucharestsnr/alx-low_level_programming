ea9231949fa9e49bea6c773ab78986410c55d3c0
Lukman Buhari <lukmanbuhari07@gmail.com>
2022-09-13 05:27:49 -0700

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
+ * main-main 
  *
  * print_alphabet - Prints the alphabet in lowercase.
- *
  *
  * Return: Always 0 (Success)
  *
  */
-
 void print_alphabet(void)
 {
 	int i;
