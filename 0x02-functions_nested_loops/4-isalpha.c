cb9b70127f71d1fa5e2c9e7293dffb48a9a753cd
Lukman Buhari <lukmanbuhari07@gmail.com>
2022-09-13 11:45:33 -0700

    m

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,24 +2,25 @@
 
 /**
  *
- * main - check the code.
+ * _isalpha - Shows 1 if the input is a
  *
- * Return: Always 0.
+ * letter Another cases, shows 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for letters. 0 for the rest.
  *
  */
 
-int main(void)
+int _isalpha(int c)
 {
-    int r;
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-        r = _isalpha('o');
-	_putchar(r + '0');
-        r = _isalpha(108);
-        _putchar(r + '0');
-        r = _isalpha(';');
-        _putchar(r + '0');
-        _putchar('\n');
-    	return (0);
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
