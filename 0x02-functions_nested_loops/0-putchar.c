a231844672e478a236eab99f024905e8ea902a8b
Lukman Buhari <lukmanbuhari07@gmail.com>
2022-09-14 06:24:04 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,10 @@
-#include "main.h"
+#include "unistd.h"
 
 /**
  * main- print putchar
  * Return: always 0
  */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
