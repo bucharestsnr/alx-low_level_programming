ffea40d4ec836b536a18b2ecb76b0e5259ad07a3
Lukman Buhari <lukmanbuhari07@gmail.com>
2022-09-15 10:13:33 -0700

    m

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9
+ * Return: print numbers
+ */
+
+void print_numbers(void)
+{
+	int ch;
+
+	for(ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
