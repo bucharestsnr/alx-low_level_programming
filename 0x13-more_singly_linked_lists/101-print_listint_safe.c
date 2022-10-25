e9b864d35c725c6b521a4b735ec66d4fbfc3df09
Lukman Buhari <lukmanbuhari07@gmail.com>
2022-10-25 02:58:28 -0700

    m

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -45,7 +45,7 @@ size_t print_listint_safe(const listint_t *head)
 		hptr = new;
 
 		add = hptr;
-																		while (add->next != NULL)
+		while (add->next != NULL)
 		{
 			add = add->next;
 			if (head == add->p)
